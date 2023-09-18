0469e679cda2e58593259f2e99c8b16b99587f8c
Andrea Selmi <andrea@start2impact.com>
2023-09-18 16:26:21 +0200

    feat: add http service

---
+++ helpers/httpService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export default http = (cookieName, loginPath) => {
+    const access_token = Cookies.get(cookieName);
+
+    const http = axios.create({
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        crossDomain: true,
+    });
+
+    http.interceptors.request.use(
+        config => {
+            config.headers.Authorization = !access_token ? null : `Bearer ${access_token}`;
+            return config;
+        },
+        error => {
+            const errorStatus = error.response ? error.response.status : null;
+
+            if (errorStatus === 401) {
+                const domain = window.location.host.includes("localhost") ? "localhost" : ".start2impact.it";
+                Cookies.remove(cookieName, { domain });
+                window.location.href = loginPath;
+            }
+
+            return Promise.reject(error);
+        },
+    );
+
+    http.interceptors.response.use(
+        response => response,
+        error => {
+            const errorStatus = error.response ? error.response.status : null;
+
+            if (errorStatus === 401) {
+                const domain = window.location.host.includes("localhost") ? "localhost" : ".start2impact.it";
+                Cookies.remove(cookieName, { domain });
+                window.location.href = loginPath;
+            }
+
+            return Promise.reject(error);
+        },
+    );
+}
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as date } from "./helpers/date";
 export { default as colors } from "./helpers/colors";
+export { default as http } from "./helpers/http";
