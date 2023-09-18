956cc65b567051bc9d8674f460ce847394ca71cf
Andrea Selmi <andrea@start2impact.com>
2023-09-18 19:34:44 +0200

    feat: add check cookie fn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as date } from "./helpers/date";
 export { default as colors } from "./helpers/colors";
 export { default as http } from "./helpers/httpService";
+export { default as checkCookie } from "./helpers/checkCookie";
