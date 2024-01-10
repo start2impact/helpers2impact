c290515855320bc3ee51634b2829eb5bdda64510
Andrea Selmi <andrea@start2impact.com>
2024-01-10 16:58:43 +0100

    ft: add redirectByRole fn

---
+++ helpers/redirectByRole.js
@@ -0,0 +1,24 @@
+import { checkCookie } from "helpers2impact";
+
+import config from "../config";
+
+
+
+export const redirectByRole = () => {
+    const { user } = checkCookie(config.cookieName);
+
+    if (!user) {
+        return;
+    }
+
+    if (user.role === "admin") {
+        window.location.href = process.env.REACT_APP_ADMIN_URL;
+    }
+
+    else if (user.role === "coach") {
+        window.location.href = process.env.REACT_APP_COACH_URL;
+    }
+    else {
+        window.location.href = process.env.REACT_APP_TALENT_URL;
+    }
+};
