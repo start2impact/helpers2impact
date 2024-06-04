91c87791eda378ed8fc67582fc073808c3c6a404
Andrea Selmi <andrea@start2impact.com>
2024-06-04 10:48:49 +0200

    .

--- helpers/array.js
+++
@@ -1,7 +0,0 @@
-const array = {
-    arrToChunks(arr = [], chuksSize = 10) {
-        return Array.from({ length: Math.ceil(arr.length / chuksSize) }, (_, i) => arr.slice(i * chuksSize, i * chuksSize + chuksSize));
-    }
-}
-
-export default array
